3426409d21371f12618b062f6e4622fbaebd1263
KingHash23 <markcalvin2003@gmail.com>
2025-01-17 17:21:18 +0300

    corrections

--- Helloserver/server.js
+++ Helloserver/server.js
@@ -1,18 +1,15 @@
+
 const express = require('express');
 
 const app = express();
 const port = 3000;
 
-
-
-//  app.get(port, (req, res) =>{
-//   res.send('Hello World!');
-//  });
-
-
+// Define a route for '/hello'
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
 
 // Middleware to parse JSON request bodies
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`)
 })
-
