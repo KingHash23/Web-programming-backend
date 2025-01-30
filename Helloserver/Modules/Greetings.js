36c5d20ca11ef4cac091bf06789abb9d0a0447ec
KingHash23 <markcalvin2003@gmail.com>
2025-01-30 11:22:49 +0300

    Modules

---
+++ Helloserver/Modules/Greetings.js
@@ -0,0 +1,9 @@
+const welcome = (name) => {
+    return `Hi ${name}. You are welcome to the system 😊🌍🦍`;
+}
+  
+  // Exporting the function as a module for use in other files.
+  module.exports = {
+    welcome
+}
+
---
+++ Helloserver/Modules/Test.js
@@ -0,0 +1,3 @@
+// how to require packages
+const { welcome } = require('./Greetings'); // Ensure Greetings.js exports 'welcome'
+console.log(welcome('Calvin'));
