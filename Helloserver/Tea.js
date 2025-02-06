ebd576329c7a7fd97eb97d3518ac28fba5eed570
KingHash23 <markcalvin2003@gmail.com>
2025-02-06 10:55:59 +0300

    timer...line by line

---
+++ Helloserver/Tea.js
@@ -0,0 +1,14 @@
+// code for boling waater
+console.log("put boiling water");
+// give it some time to boil for like 5 seconds
+
+// code for  water  is boling
+console.log("water is boiling");
+
+// timer for water to boil for 5 seconds
+setTimeout(() => {
+    console.log("water is ready");
+}, 5000);
+
+console.log("Do other things....");
+    
