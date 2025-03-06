293f9d8354016fc2da248afa10f8c54c205eb50f
KingHash23 <markcalvin2003@gmail.com>
2025-03-06 12:36:35 +0300

    Mysql introduction

---
+++ HelloServer2/config.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+require('dotenv').config()
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT
+})
+ db.connect((err) => {
+    if (err) throw err;
+    console.log('Database is connected');
+})
+ module.exports = db;
--- HelloServer2/index.js
+++ HelloServer2/index.js
@@ -1,9 +1,19 @@
 
 const express = require('express');
-
+const db = require('./config')
 const app = express();
+app.use(express.json());
 const port = 3000;
 
+const sql = `Create Table if not exists users (
+            id int auto_increment primary key, 
+            name varchar(15), 
+            email varchar(10))`;
+
+db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log("Table created");
+});
 // logging middleware  function
 
 function loggingMiddleware(req, res, next) {
@@ -21,6 +31,21 @@ app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
+app.post('/add-user', (req, res) => {
+    const { name, email } = req.body;
+    const sql = `INSERT INTO users (name, email) VALUES (?,?)`;
+    db.query(sql, [name, email], (err, result) => {
+        if (err) throw err;
+        res.send('User added successfully');
+    });
+});
+app.get('/users', (req, res) => {
+    const sql = `SELECT * FROM users`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
 // Middleware to parse JSON request bodies
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`)
